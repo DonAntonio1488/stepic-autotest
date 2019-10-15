5da95e6cf9c7b4a0e191ac54e9d4211a358100cb
DonAntonio1488 <tatosha-89@inbox.ru>
2019-10-15 21:26:39 +0300

    последнее задание второго этапа

---
+++ капча.py
@@ -0,0 +1,42 @@
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+from selenium import webdriver
+import time
+
+try: 
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+	
+    # Нажимаем на кнопку  и переходим на новую вкладку
+    button1 = browser.find_element_by_css_selector("button.btn").click()
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+    
+    # Ищем х и вводим ответ в поле
+    x_element = browser.find_element_by_id("input_value")
+    x = int(x_element.text)
+    y = calc(x)
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+	
+    
+    # Отправляем заполненную форму
+    button2 = browser.find_element_by_css_selector("button.btn")
+    button2.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    
+
+    
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
